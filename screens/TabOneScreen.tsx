d3892f03c204bd039b4bfc7ca729b830e315dab0
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2021-07-12 17:51:57 -0300

    init

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,10 +1,45 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Linking, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+/* 
+import * as FS from 'expo-file-system' */
+
+import {StorageAccessFramework} from 'expo-file-system'
 
 export default function TabOneScreen() {
+
+  const fileS = async () =>{
+
+    Linking.getInitialURL().then((url) => {
+      if (url) {
+            console.log(url);
+        }
+    
+    }).catch(err => console.error('An error occurred', err));
+
+    // Requests permissions for external directory
+    const permissions = await StorageAccessFramework.requestDirectoryPermissionsAsync();
+  
+    if (permissions.granted) {
+      // Gets SAF URI from response
+      const uri = permissions.directoryUri;
+  
+      // Gets all files inside of selected directory
+      const files = await StorageAccessFramework.readDirectoryAsync(uri);
+      alert(`Files inside ${uri}:\n\n${JSON.stringify(files)}`);
+    }
+
+
+    //const uri = await FS.readDirectoryAsync('content://com.android.providers.downloads.documents/tree/downloads')
+    //const uri = await FS.readAsStringAsync('file:///private/var/mobile/Containers/Data/Application/', { encoding: FS.EncodingType.Base64 })
+  }
+
+  React.useEffect(() => {
+    fileS()
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
